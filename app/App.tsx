6d7a6a518773e7487685de176a5abf260631c266
Breno Oliveira <breno.oliveira@avancoinfo.com.br>
2021-08-09 14:20:17 -0300

    Achado

--- app/App.tsx
+++ app/App.tsx
@@ -1,64 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import { Skeleton } from 'react-native-preload';
 
+const App = () => {
 
-import React, { Component, memo, ReactNode } from 'react';
-import { Text, View, Button, StyleSheet, ScrollView, FlatList } from 'react-native';
+  const [loading, setLoading] = useState(true);
 
-
-import Slider from '@react-native-community/slider';
-interface IAppState {
-  valor: number;
-}
-
-
-interface IAppProps {
-  nome?: string
-}
-
-export default class App extends Component<IAppProps, IAppState> {
-
-  constructor(props: any) {
-    super(props)
-    this.state = {
-      valor: 0
-    }
+  const carregando = (delay: number, valor: boolean) => {
+    setTimeout(() => setLoading(valor), delay);
   }
 
+  useEffect(() => carregando(3000, false));
 
-  render() {
-    return (
-      <View style={style.container}>
-        <Slider
-          minimumValue={0}
-          maximumValue={100}
-          onValueChange={(valor: number) => this.setState({ valor })}
-          value={this.state.valor}
-          minimumTrackTintColor="blue"
-          maximumTrackTintColor="red"
-          thumbTintColor="yellow"
-        />
-        <Text style={{fontSize: 20, textAlign: 'center', marginTop: 40}}>{this.state.valor}</Text>
-      </View>
+  return (
 
-
-    )
-  }
+    <View>
+      {loading == true ? <Skeleton  Width={170} Height={130} BorderRadius={10} /> : <Text>Hello World</Text>}
+    </View>
+  )
 }
 
-
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 20,
-  },
-  logo: {
-    textAlign: 'center',
-    fontSize: 28,
-    fontWeight: 'bold'
-  },
-  pizzas: {
-    marginTop: 15,
-    fontSize: 25,
-    textAlign: 'center'
-  }
-})
-
+export default App;
